16979f6bde4dcefc65fb142686037b247cc8ad3c
Delisoal <delisoal417@gmail.com>
2025-11-23 23:43:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 function id(id){
   return document.getElementById(id);
 }
+let minArray=[];
+let maxArray=[];
 function setMP(){
-  
+  fetch("./embed/"+id("mpname").value).then(function(data){
+    return data.text();
+  }).then(function(html){
+    id("config").innerHTML=html;
+    setEmbed();
+  });
 }
+setMP();
+id("mpname").oninput=setMP;
+function setEmbed(){
+  minArray=maxArray=[];
+  document.querySelectorAll("input,select").forEach(function(element){
+    let min=element.value.split(",")[0];
+    let max=element.value.split(",")[1];
+    min?minArray[minArray.length]=min:"";
+    max?maxArray[maxArray.length]=max:"";
+  });
+  let min=minArray.sort(function(a,b){return a-b})[0];
+  let max=maxArray.sort(function(a,b){return b-a})[0];
+  console.log(min,max);
+}
+id("config").oninput=setEmbed();
